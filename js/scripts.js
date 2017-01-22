5663c0577d9ebde83ebb29b8fe0a905803e69677
Justin Stofiel <jstofiel@mac.com>
2017-01-22 14:44:53 -0800

    basic logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,23 @@
 $(function() {
-  $('form#').submit(function(event) {
+  $('form#form1').submit(function(event) {
     event.preventDefault();
+    parseInt($('input#input-user').val());
+    var userInput = $("input#input-user").val();
+    divisible(userInput);
+  });
+});
+
+// UI above^^
+
+// Logic below
+function divisible(userInput) {
+    if( userInput % 3 == 0 ) {
+        alert("ping");
+    }
+    if( userInput % 5 == 0 ) {
+        alert("pong");
+    }
+    if( userInput % 15 == 0 ) {
+        alert("ping-pong");
+    }
+  }
